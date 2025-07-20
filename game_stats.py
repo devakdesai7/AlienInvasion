57a117f623cf1814420d6e49f5c5080f7728ef08
devakdesai7 <216946260+devakdesai7@users.noreply.github.com>
2025-07-20 16:03:13 +0530

    Added the button class to create a button and display a msg on it

--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,7 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self._reset_stats()
-        self.game_active = True
+        self.game_active = False #making game_active = False because we want the game loop to run after we press the play button
 
     def _reset_stats(self):
         self.ships_left = self.settings.ship_limit
