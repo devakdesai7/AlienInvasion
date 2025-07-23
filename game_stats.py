d0d278c4d1a42c7802c70d75dc322810344fcfa2
devakdesai7 <216946260+devakdesai7@users.noreply.github.com>
2025-07-23 17:36:39 +0530

    Added code to display high score in the game

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,7 @@ class GameStats:
         self.settings = ai_game.settings
         self._reset_stats()
         self.game_active = False #making game_active = False because we want the game loop to run after we press the play button
+        self.high_score = 0
 
     def _reset_stats(self):
         self.ships_left = self.settings.ship_limit
--- scoreboard.py
+++ scoreboard.py
@@ -13,6 +13,7 @@ class Scoreboard:
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Create the image of scoreboard to display on screen"""
@@ -21,10 +22,28 @@ class Scoreboard:
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         #Display the score at the top right corner of the screen
-        self.rect = self.score_image.get_rect()
-        self.rect.right = self.screen_rect.right - 20
-        self.rect.top = 20
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def prep_high_score(self):
+        """Create the image of scoreboard to display on screen"""
+        rounded_high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(rounded_high_score)
+        self.high_score_image = self.font.render(f"High Score: {high_score_str}", True, self.text_color, self.settings.bg_color)
+
+        #Display the high score at the top right corner of the and below the current score
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
 
     def show_score(self):
         """Draw score to the screen"""
-        self.screen.blit(self.score_image, self.rect)
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
